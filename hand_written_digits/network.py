c061323c7cf3ca51903e9c6d8fcee73885fd3e50
Mehmet Hilmi CAN <mehmet.hilmi@marun.edu.tr>
2024-09-01 21:40:24 +0300

    Update network.py

--- hand_written_digits/network.py
+++ hand_written_digits/network.py
@@ -10,7 +10,7 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 import gzip
 import pickle
 
-with gzip.open('data/mnist.pkl.gz', 'rb') as f:
+with gzip.open('mnist.pkl.gz', 'rb') as f:
     train_set, valid_set, test_set = pickle.load(f, encoding='latin1')
 
 x_train, y_train = train_set
